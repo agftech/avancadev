4760dcb37733c0839e48c281b60b51c8283cdde2
agftech <agiraof@gmail.com>
2020-11-04 13:54:11 -0800

    :package: feat: finish msa microservice with rabbitmq, docker and go

--- microservices/go.mod
+++ microservices/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/hashicorp/go-retryablehttp v0.6.7
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/wesleywillians/go-rabbitmq v0.0.0-20201027193450-55e6a80937af // indirect
 )
--- microservices/msa/msa.go
+++ microservices/msa/msa.go
@@ -3,14 +3,17 @@ package main
 import (
 	"encoding/json"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 
-	"github.com/hashicorp/go-retryablehttp"
 	"github.com/joho/godotenv"
+	"github.com/wesleywillians/go-rabbitmq/queue"
 )
+
+type Order struct {
+	Coupon   string
+	CcNumber string
+}
 
 type Result struct {
 	Status string
@@ -35,37 +38,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
+	coupon := r.PostFormValue("coupon")
+	ccNumber := r.PostFormValue("cc-number")
 
-	result := makeHttpCall("http://localhost:9091", r.FormValue("coupon"), r.FormValue("cc-number"))
-
-	t := template.Must(template.ParseFiles("templates/home.html"))
-	t.Execute(w, result)
-}
-
-func makeHttpCall(urlMicroservice string, coupon string, ccNumber string) Result {
-
-	values := url.Values{}
-	values.Add("coupon", coupon)
-	values.Add("ccNumber", ccNumber)
-
-	retryClient := retryablehttp.NewClient()
-	retryClient.RetryMax = 5
-
-	res, err := retryClient.PostForm(urlMicroservice, values)
-	if err != nil {
-		result := Result{Status: "Servidor fora do ar!"}
-		return result
+	order := Order{
+		Coupon:   coupon,
+		CcNumber: ccNumber,
 	}
 
-	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
+	jsonOrder, err := json.Marshal(order)
 	if err != nil {
-		log.Fatal("Error processing result")
+		log.Fatal("Error parsing to json")
 	}
 
-	result := Result{}
+	rabbitMQ := queue.NewRabbitMQ()
+	ch := rabbitMQ.Connect()
+	defer ch.Close()
 
-	json.Unmarshal(data, &result)
+	err = rabbitMQ.Notify(string(jsonOrder), "application/json", "orders_ex", "")
+	if err != nil {
+		log.Fatal("Error sending message to the queue")
+	}
 
-	return result
+	t := template.Must(template.ParseFiles("templates/process.html"))
+	t.Execute(w, "")
 }
